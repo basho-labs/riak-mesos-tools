ca0aa6738c78c6dbff8c80007017fec196b178e9
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-01-13 01:08:00 -0500

    fixing case_selector

--- riak_mesos/cli.py
+++ riak_mesos/cli.py
@@ -176,11 +176,8 @@ class CliError(Exception):
 
 
 class case_selector(Exception):
-    def __init__(self,value):
-        self.value=value
-
-    def __str__(self):
-        return repr(self.value)
+    def __init__(self, value):
+          Exception.__init__(self, value)
 
 
 def switch(variable):
