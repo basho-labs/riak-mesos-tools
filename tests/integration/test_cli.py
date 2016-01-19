76b041a5fee6591b307ac106b173baeaaf86df5b
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-01-18 21:30:54 -0500

    test fix

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -6,7 +6,7 @@ from common import exec_command as _c
 def test_version():
     c, o, e = _c(['dcos-riak', '--version'])
     assert c == 0
-    assert o.strip() == b'Riak Mesos Framework Version 0.3.0'
+    assert o.strip() == b'Riak Mesos Framework Version 0.3.1'
     assert e == b''
 
 
